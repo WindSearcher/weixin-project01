5565b2227ad176fbf953381f9d0fee753f697aaf
zlc18 <2827212797@qq.com>
2019-09-12 18:37:52 +0800

    Plan page and index page have been modified_2

--- pages/addPlan/addPlan.js
+++ pages/addPlan/addPlan.js
@@ -3,10 +3,13 @@ var app = getApp();
 Page({
 
   data: {
-    plan: ''
+    allCompleted: true,
+    plan: '',
+    plan_item: '',
   },
 
   getPlan: function(e) {
+
     this.setData({
       plan: e.detail.value
     })
@@ -28,13 +31,19 @@ Page({
               icon: 'success',
               duration: 2000
             })
+
             wx.hideLoading()
             that.setData({
               plan: ''
             })
+            console.log(res.data);
+            that.onShow()
+
+
           })
         }
       })
+
     } else {
       wx.showModal({
         title: '计划为空',
@@ -45,4 +54,53 @@ Page({
       })
     }
   },
+  //获取所选日期计划
+  onShow: function() {
+    var that = this
+    var date = wx.getStorageSync("selectedDate")
+    that.setData({
+      selectedDate: date
+    })
+    wx.getUserInfo({
+      success: res => {
+        app.ajax.post('/plan/get', {
+          nickName: res.userInfo.nickName,
+          date: date,
+        }, function(res) {
+          that.setData({
+            plan_item: res.data,
+          })
+          console.log(that.data.plan_item)
+          console.log(date)
+          console.log(res.data);
+
+        })
+      }
+    })
+  },
+
+  filterCompleted: function() {
+    var that = this;
+    var {
+      plan_item,
+    } = that.data;
+    var remain = plan_item.filter(function(todo) {
+      return todo.status == false;
+    });
+
+    if (remain.length < plan_item.length) {
+      wx.showModal({
+        title: '提示',
+        content: '显示今天未完成的计划？',
+        success: function(res) {
+          if (res.confirm) {
+
+            that.setData({
+              plan_item: remain
+            })
+          }
+        }
+      });
+    }
+  }
 })
